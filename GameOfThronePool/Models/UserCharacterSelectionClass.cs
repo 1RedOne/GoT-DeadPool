da5c65a69e905c7fa37c7c34cd99fd7eb0975cb0
1RedOne <Stephen@foxdeploy.com>
2019-03-27 14:11:04 -0400

    login working

---
+++ GameOfThronePool/Models/UserCharacterSelectionClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GameOfThronePool.Models
+{
+    public class UserCharacterSelection
+    {
+        public int UserID { get; set; }
+        public int UserCharacterSelectionID { get; set; }
+        public int CharacterID { get; set; }
+        public string CharacterName { get; set; }
+        public bool AliveStatus { get; set; }
+        public bool BecomesAWhiteWalker { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}
